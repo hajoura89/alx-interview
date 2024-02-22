24e49fc4fd5d3683dc6c5bf4f6796359e709e693
hajoura89 <hajarkammou@gmail.com>
2024-02-22 14:40:35 -0800

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,22 +2,31 @@
 """The Minimum Operations Challenge"""
 
 
-def minOperations(n):
-    if n == 1:
-        return 0  # Already have one 'H'
+def min_operations_to_get_n_h(n):
+    """Computes the minimum operations"""
+    if not isinstance(n, int):
+        return 0
 
-    operations = 0
-    clipboard = 0
+    operations_count = 0
+    clipboard_size = 0
+    current_characters = 1
 
-    for i in range(2, n + 1):
-        if n % i == 0:
-            clipboard = i
-            operations += 2  # Copy All and Paste
+    while current_characters < n:
+        if clipboard_size == 0:
+            # Initialize clipboard and perform the
+            # first copy all and paste operation
+            clipboard_size = current_characters
+            current_characters += clipboard_size
+            operations_count += 2
+        elif n - current_characters > 0 and \
+                (n - current_characters) % current_characters == 0:
+            # Copy all and paste operation
+            clipboard_size = current_characters
+            current_characters += clipboard_size
+            operations_count += 2
+        elif clipboard_size > 0:
+            # Paste operation
+            current_characters += clipboard_size
+            operations_count += 1
 
-            while n % clipboard == 0:
-                n //= clipboard
-                operations += 1  # Paste
-
-    return operations if n == 1 else 0
-
-
+    return operations_count
